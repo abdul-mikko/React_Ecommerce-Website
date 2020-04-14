2447e7dd2cca934876c757f10cb7ec697595eedc
Adil <abdul.adil.cs@gmail.com>
2020-04-14 16:24:07 -0700

    Ecommerce

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HomePage from './HomePage-Component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
