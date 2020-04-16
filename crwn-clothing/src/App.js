03404792002fc583798fc82bf7df88cc89c51dcf
Adil <abdul.adil.cs@gmail.com>
2020-04-16 09:01:06 -0700

    SIgn in and route setup

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import HomePage from './Pages/Homepage/HomePage-Component';
 import './App.css';
+import HomePage from './Pages/Homepage/HomePage-Component';
 import CollectionArray from './Pages/Collection/Collection-Array-Components'
 import Header from '../src/Components/Header/Header.component'
-
+import Sign_In_And_Sign_Up from './Pages/Sign_In_And_Sign_Up/Sign_In_And_Sign_Up'
 
 
 
@@ -17,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={CollectionArray} />
-
+        <Route path='/signin' component={Sign_In_And_Sign_Up} />
       </Switch>
     </div>
   );
---
+++ crwn-clothing/src/Pages/Sign_In_And_Sign_Up/Sign_In_And_Sign_Up.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sign_In_And_Sign_Up.style.scss'
+
+export default SignInandSignUp = () => {
+    return (
+        <div>
+            <h1>SIGN IN</h1>
+
+        </div>
+
+    )
+}
